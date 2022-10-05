59092a915aafdd074deb69a2d974c5df63ccd972
EdViTeR <maxpivnenko@mail.ru>
2022-10-05 17:52:08 +0300

    первая страница

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="assets/css/home/home.css">
+	<title>Главная</title>
+</head>
+<body>
+
+<header class="header">
+	<div class="header_inner">
+		<div class="header_logo">
+			<img class="logo_inner_img" src="assets/img/home/logo.svg">
+		</div>
+		<nav>
+			<a href="">Главная</a>
+			<a href="">Курсы</a>
+			<a href="">Новости</a>
+			<a href="">Афиша</a>
+			<a href="">Ещё</a>
+		</nav>
+	</div>
+</header>
+
+<div class="intro">
+	<div class="container">
+		
+	</div>
+</div>
+
+
+</body>
+</html>
