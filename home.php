6f86f3b99c1b95a3f123bdb8b081efb633ec7933
EdViTeR <maxpivnenko@mail.ru>
2023-01-19 17:58:42 +0300

    почти шапка

--- home.php
+++ home.php
@@ -7,28 +7,35 @@
 	<title>Главная</title>
 </head>
 <body>
-
-<header class="header">
-	<div class="header_inner">
-		<div class="header_logo">
-			<img class="logo_inner_img" src="assets/img/home/logo.svg">
-		</div>
-		<nav>
-			<a href="">Главная</a>
-			<a href="">Курсы</a>
-			<a href="">Новости</a>
-			<a href="">Афиша</a>
-			<a href="">Ещё</a>
-		</nav>
-	</div>
-</header>
-
-<div class="intro">
 	<div class="container">
-		
-	</div>
-</div>
+		<header class="header">
+			<div class="header__inner">
+				<div class="header__logo">
+					<a class="nav__link active" href="#">Главная</a>
+				</div>
+				<nav class="nav">
+					<a class="nav__link active" href="#">Главная</a>
+					<a class="nav__link" href="#">Курсы</a>
+					<a class="nav__link" href="#">Новости</a>
+					<a class="nav__link" href="#">Личный кабинет</a>
+					<a class="nav__link" href="#">EDU</a>
+				</nav>
+			</div>
+		</header>
+	</div>	
 
+	<div class="intro">
+		<div class="container">
+			<div class="rectangle">
+				<div class="rectangle_title">Технологии 3D-печати для медицины</div>
+				<div class="rectangle_text">В курсе будет продемонстрировано как новая технология 3D-принтирования способствует развитию инновационных направлений медицины будущего: тканевой инженерии, регенеративной медицины, созданию систем контролируемого высвобождения лекарственных соединений.</div>
+				<div class="printer__logo"></div>
+				<div class="rectangle__button">
+					<div class="rectangle__button__text">Записаться на курс</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,126 @@
+<!-- canIuse -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mogo</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<header class="header">
+		<div class="container">
+			<div class="header__inner">
+				<div class="header__logo">MoGo</div>
+
+				<nav class="nav">
+					<a class="nav__link active" href="#">About</a>
+					<a class="nav__link" href="#">Services</a>
+					<a class="nav__link" href="#">Blog</a>
+					<a class="nav__link" href="#">Work</a>
+					<a class="nav__link" href="#">Contact</a>
+				</nav>
+			</div>
+		</div>	
+	</header>
+
+	<div class="intro">
+		<div class="container">
+			<div class="intro__inner">
+				<h2 class="intro__suptitle">Creative Template</h2>
+				<h1 class="intro__title">Welcome to MoGo</h1>
+
+				<a class="btn" href="#">Learn More</a>
+			</div>
+		</div>
+		<div class="slider">
+			<div class="container">
+				<div class="slider_inner">
+					<div class="slider_item active">
+						<span class="slider_num">01</span>
+						Intro
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">02</span>
+						Work
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">03</span>
+						About
+					</div>
+					<div class="slider_item">
+						<span class="slider_num">04</span>
+						Contacts
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<section class="section">
+		<div class="container">
+			<div class="section__header">
+				<h3 class="section__suptitle">What we do</h3>
+				<h2 class="section__title">Story about us</h2>
+				<div>
+					<p class="section__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+			</div>
+
+			<div class="about">
+				<div class="about__item">
+					<div class="about__img">
+						<img src="assets/images/about/1.jpg">
+					</div>
+					<div class="about__text">Super team</div>
+				</div>
+				<div class="about__item">
+					<div class="about__img">
+						<img src="assets/images/about/2.jpg">
+					</div>
+					<div class="about__text">Super team</div>
+				</div>
+				<div class="about__item">
+					<div class="about__img">
+						<img src="assets/images/about/3.jpg">
+					</div>
+					<div class="about__text">Super team</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="statistics">
+		<div class="container">
+			<div class="stat">
+				<div class="stat__item">
+					<div class="stat__count">42</div>
+					<div class="stat__text">Web design projects</div>
+				</div>
+				<div class="stat__item">
+					<div class="stat__count">123</div>
+					<div class="stat__text">Happy client</div>
+				</div>
+				<div class="stat__item">
+					<div class="stat__count">15</div>
+					<div class="stat__text">Award winner</div>
+				</div>
+				<div class="stat__item">
+					<div class="stat__count">99</div>
+					<div class="stat__text">Cup of coffee</div>
+				</div>
+				<div class="stat__item">
+					<div class="stat__count">24</div>
+					<div class="stat__text">Members</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<footer>
+	<a class="freepik" href="https://ru.freepik.com/free-vector/3d-printing-orthogonal-icons_4300988.htm#query=3d%20%D0%BF%D1%80%D0%B8%D0%BD%D1%82%D0%B5%D1%80&position=18&from_view=search&track=sph">Изображение от macrovector на Freepik</a>
+</footer>
+
+</body>
+</html>
